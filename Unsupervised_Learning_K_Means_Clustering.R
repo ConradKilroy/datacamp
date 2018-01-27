0e743396a8fb8c7a5db59d6548e18ef05c44fe87
ConradKilroy <conradkilroy@gmail.com>
2018-01-27 15:32:26 -0800

    Update Unsupervised_Learning_K_Means_Clustering.R

--- Unsupervised_Learning_K_Means_Clustering.R
+++ Unsupervised_Learning_K_Means_Clustering.R
@@ -17,6 +17,8 @@ plot(my_data)
 ########
 set.seed(1) #VERY IMPORTANT for repeatability, cluster colors wont change!!!!
 # Create the k-means model: km.out
+#number of centers
+#number of iterations
 km.out <- kmeans(x = my_data, centers = 3, nstart = 20)
 
 # Inspect the result
@@ -78,6 +80,41 @@ plot(1:15, wss, type = "b",
      ylab = "Within groups sum of squares",
      main = "Scree Plot")
 
-
-
 ###SCREE PLOT, the point when the number of clusters
+
+
+
+#---------------------------------
+#Pokemon practice
+#datafile: pokemon.csv
+pokemon <- read.csv("data/pokemon.csv", header = TRUE, sep = ",")
+# Initialize total within sum of squares error: wss
+wss <- 0
+
+# Look over 1 to 15 possible clusters
+for (i in 1:15) {
+  # Fit the model: km.out
+  km.out <- kmeans(pokemon, centers = i, nstart = 20, iter.max = 50)
+  # Save the within cluster sum of squares
+  wss[i] <- km.out$tot.withinss
+}
+
+# Produce a scree plot
+plot(1:15, wss, type = "b", 
+     xlab = "Number of Clusters", 
+     ylab = "Within groups sum of squares")
+
+# Select number of clusters
+k <- 2
+
+# Build model with k clusters: km.out
+km.out <- kmeans(pokemon, centers = k, nstart = 20, iter.max = 50)
+
+# View the resulting model
+km.out
+
+# Plot of Defense vs. Speed by cluster membership
+plot(pokemon[, c("Defense", "Speed")],
+     col = km.out$cluster,
+     main = paste("k-means clustering of Pokemon with", k, "clusters"),
+     xlab = "Defense", ylab = "Speed")
